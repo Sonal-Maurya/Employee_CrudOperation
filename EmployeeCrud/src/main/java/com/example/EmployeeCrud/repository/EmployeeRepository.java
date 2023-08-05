92d1ab0d0d1ed7d729ad9d8cc710da5311c49e9b
Sonal Maurya <86039603+Sonal-Maurya@users.noreply.github.com>
2023-08-06 01:09:47 +0530

    Add files via upload
    Main file

---
+++ EmployeeCrud/src/main/java/com/example/EmployeeCrud/repository/EmployeeRepository.java
@@ -0,0 +1,15 @@
+package com.example.EmployeeCrud.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.EmployeeCrud.model.Employee;
+
+
+
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee,Long> {
+	
+
+}
